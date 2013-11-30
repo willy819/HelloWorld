fc8309a12cdbe6000bc34d5dd523adee92bab8d3
dosukyou <dosukyou@gmail.com>
2013-11-30 06:21:01 -0500

    index test

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@
 var app = {
     // Application Constructor
     initialize: function() {
+		$('#listening').hide();
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -33,17 +34,21 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+	alert('test');
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
+      
+	//	var parentElement = document.getElementById(id);
+    //   var listeningElement = parentElement.querySelector('.listening');
+    //    var receivedElement = parentElement.querySelector('.received');
+    //    listeningElement.setAttribute('style', 'display:none;');
+     //   receivedElement.setAttribute('style', 'display:block;');
+		
+		$('#received').show();
+		
+				
         console.log('Received Event: ' + id);
     }
 };
